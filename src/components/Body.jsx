ba45e6c684ff6b5fde729b1c24ef9566870409c3
vamsi-88 <vamsikanna369@gmail.com>
2026-01-12 10:27:06 +0530

    Token Valid related Redirect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,41 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import BASE_URL from '../utils/constants'
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
+import { useEffect } from 'react'
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const fetchUser = async ()=>{
+      try{
+        const res = await axios.get(BASE_URL+"/profile/view" ,
+        {
+          withCredentials : true,
+        })
+
+        dispatch(addUser(res.data));
+      }catch(err){
+        if(err.status === 401){
+        navigate("/login")
+      }
+        console.log(err.message);
+      }
+    }
+
+
+    useEffect(()=>{
+      fetchUser();
+    } ,[ ]);
+
+
+
+
   return (
     <>
       <Navbar/>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
 import { useNavigate } from 'react-router-dom';
+import BASE_URL from '../utils/constants';
 
 const Login = () => {
 
@@ -15,7 +16,7 @@ const Login = () => {
 
     const handleChange = async( ) => {
         try{
-        const res = await axios.post("http://localhost:7777/login",{
+        const res = await axios.post(BASE_URL+"/login",{
             emailId,
             password
         },
