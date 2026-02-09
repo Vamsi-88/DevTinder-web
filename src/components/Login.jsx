32b5de7640cb3465b224f10248f8b542def3d9a3
vamsi-88 <vamsikanna369@gmail.com>
2026-02-09 11:35:08 +0530

    Added SignIn form in loginIn Form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../utils/userSlice';
@@ -7,67 +6,121 @@ import { useNavigate } from 'react-router-dom';
 import BASE_URL from '../utils/constants';
 
 const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const[emailId,setEmailId] = useState("eren@gmail.com");
-    const[password,setpassword] = useState("Eren@1234");
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [error,setError] = useState("");
-
-
-    const handleChange = async( ) => {
-        try{
-        const res = await axios.post(BASE_URL+"/login",{
-            emailId,
-            password
-        },
-        {withCredentials : true}
-        )
-        navigate("/");
-
-        dispatch(addUser(res.data))
-        }catch(err){
-            setError(err?.response?.data || "something went wrong")
-        }
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        { emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      navigate("/");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
     }
+  };
 
-
-
-
-
+  const handleSignup = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
 
   return (
-    <div className='flex justify-center my-10'>
-        <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-100 w-96 shadow-xl">
         <div className="card-body items-center text-center">
-            <h2 className="card-title">Login</h2>
+          <h2 className="card-title">{isLogin ? "Login" : "Signup"}</h2>
 
+          {!isLogin && (
+            <>
+              <label className="form-control w-full max-w-xs text-left">
+                <div className="label py-2">
+                  <span className="label-text">First Name</span>
+                </div>
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </label>
 
-            <label className="form-control w-full max-w-xs text-left">
+              <label className="form-control w-full max-w-xs text-left">
+                <div className="label py-2">
+                  <span className="label-text">Last Name</span>
+                </div>
+                <input
+                  type="text"
+                  className="input input-bordered w-full max-w-xs"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </label>
+            </>
+          )}
+
+          <label className="form-control w-full max-w-xs text-left">
             <div className="label py-2">
-                <span className="label-text">Email Id</span>
+              <span className="label-text">Email Id</span>
             </div>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs " value={emailId} onChange={(e)=>setEmailId(e.target.value)}/>
-            </label>
+            <input
+              type="text"
+              className="input input-bordered w-full max-w-xs"
+              value={emailId}
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </label>
 
-
-            <label className="form-control w-full max-w-xs text-left">
+          <label className="form-control w-full max-w-xs text-left">
             <div className="label py-2">
-                <span className="label-text">Password</span>
+              <span className="label-text">Password</span>
             </div>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs " value={password} onChange={(e)=>setpassword(e.target.value)} />
-            </label>
+            <input
+              type="password"
+              className="input input-bordered w-full max-w-xs"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
 
-            <div className="card-actions">
-                <p className='text-red-500'>{error}</p>
-            <button className="btn btn-primary" onClick={handleChange}>Login</button>
-            </div>
-        </div>
-        </div>
+          <div className="card-actions">
+            <p className="text-red-500">{error}</p>
+            <button
+              className="btn btn-primary"
+              onClick={isLogin ? handleLogin : handleSignup}
+            >
+              {isLogin ? "Login" : "Signup"}
+            </button>
+          </div>
 
+          <p
+            className=" cursor-pointer py-3"
+            onClick={() => setIsLogin((value) => !value)}
+          >
+            {isLogin ? "New user? Signup Here" : "Existing user? Login Here"}
+          </p>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
